11bcf6bef018f957825916af3f4135e5ffad1aa5
Bhavartha Khawale <bhavarthakhawale@gmail.com>
2020-09-16 16:57:17 +0530

    Search weather by place name

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-page class="flex column">
+  <q-page class="flex column" :class="bgClass">
     <div class="col q-pt-lg q-px-md">
-      <q-input v-model="search" placeholder="City" dark borderless>
+      <q-input @keyup.enter="getWeatherByPlace" v-model="search" placeholder="City" dark borderless>
         <template v-slot:prepend>
-          <q-icon @click="getLocation" name="my_location" />
+          <q-icon @click="getWeatherByCoordinates" name="my_location" />
         </template>
         <template v-slot:append>
-          <q-icon name="search" @click="text = ''" class="cursor-pointer" />
+          <q-icon name="search" @click="getWeatherByPlace" class="cursor-pointer" />
         </template>
       </q-input>
     </div>
@@ -15,24 +15,21 @@
         <div class="text-h4 text-weight-light">{{weatherData.name}}</div>
         <div class="text-weight-light">{{weatherData.weather[0].main}}</div>
         <div class="q-my-lg relative-position">
-          <span class="text-weight-thin text-h1">{{weatherData.main.temp}}</span>
+          <span class="text-weight-thin text-h1">{{Math.round(weatherData.main.temp)}}</span>
           <span class="text-weight-thin text-h3 degree">&deg;</span>
         </div>
       </div>
 
       <div class="col text-center">
-        <img src="https://picsum.photos/100" alt="Pic" />
+        <img :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`" />
       </div>
     </template>
     <template v-else>
-      <div class="col column text-center text-white">
-        <div class="col text-h2 text-weight-thin">
-          Quarar
-          <br />Weather
-        </div>
-        <q-btn class="col" flat @click="getLocation">
+      <div class="col text-center text-white">
+        <div class="col text-h2 text-weight-thin q-mb-xl">Weather</div>
+        <q-btn class="col" flat @click="getWeatherByCoordinates">
           <q-icon left size="3" name="my_location" />
-          <div>Find my location</div>
+          <div>Get weather</div>
         </q-btn>
       </div>
     </template>
@@ -52,19 +49,42 @@ export default {
       lon: null,
     };
   },
+  computed: {
+    bgClass() {
+      if (this.weatherData) {
+        if (this.weatherData.weather[0].icon.endsWith("n")) {
+          return "bg-night";
+        } else {
+          return "bg-day";
+        }
+      }
+    },
+  },
   methods: {
     getLocation() {
+      this.$q.loading.show();
       navigator.geolocation.getCurrentPosition((position) => {
         this.lat = position.coords.latitude;
         this.lon = position.coords.longitude;
-        this.getWeatherByCoordinates();
       });
+      this.$q.loading.hide();
+    },
+    async getWeatherByPlace() {
+      this.$q.loading.show();
+      const res = await this.$axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.search}&appid=${this.apiKey}&units=metric`
+      );
+      this.weatherData = res.data;
+      this.$q.loading.hide();
     },
     async getWeatherByCoordinates() {
+      this.$q.loading.show();
+      this.getLocation();
       const res = await this.$axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`
       );
       this.weatherData = res.data;
+      this.$q.loading.hide();
     },
   },
 };
@@ -72,7 +92,13 @@ export default {
 
 <style lang="scss" scoped>
 .q-page {
-  background: linear-gradient(to right, #136a8a, #267871);
+  background: linear-gradient(to bottom, #136a8a, #267871);
+  &.bg-day {
+    background: linear-gradient(to bottom, #fdc830, #f37335);
+  }
+  &.bg-night {
+    background: linear-gradient(to bottom, #232526, #414345);
+  }
 }
 .degree {
   position: absolute;
